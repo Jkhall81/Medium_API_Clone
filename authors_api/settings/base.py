fac02b99efc5ec681026514c656992ad5154e34c
Jkhall81 <jason.kei.hall@gmail.com>
2023-11-27 13:01:42 -0700

    set up celery, redis, and flower.  This app now has seven containers.  manipulating them has been extremely easy using makefile commands.  lots of cool stuff going on.

--- authors_api/settings/base.py
+++ authors_api/settings/base.py
@@ -35,6 +35,7 @@ THIRD_PARTY_APPS = [
     'phonenumber_field',
     'drf_yasg',
     'corsheaders',
+    'djcelery_email',
 ]
 
 LOCAL_APPS = [
@@ -153,6 +154,17 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 CORS_URLS_REGEX = r"^api/.*$"
 
 AUTH_USER_MODEL = "users.User"
+
+CELERY_BROKER_URL = env('CELERY_BROKER')
+CELERY_RESULT_BACKEND = CELERY_BROKER_URL
+CELERY_ACCEPT_CONTENT = ['json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
+CELERY_RESULT_BACKEND_MAX_RETRIES = 10
+CELERY_TASK_SEND_SENT_EVENT = True
+
+if USE_TZ:
+    CELERY_TIMEZONE = TIME_ZONE
 
 LOGGING = {
     "version": 1,
