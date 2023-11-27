09581ebc7928b119efdb94c552c552518b6f855b
Jkhall81 <jason.kei.hall@gmail.com>
2023-11-26 18:25:42 -0700

    switched api documentation from redoc to swagger, i like the swagger ui better.  set up nginx container, added it to local.yml.  set up nginx default.conf.

--- authors_api/urls.py
+++ authors_api/urls.py
@@ -18,7 +18,7 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
-    path("redoc/", schema_view.with_ui("redoc", cache_timeout=0)),
+    path("swagger/", schema_view.with_ui("swagger", cache_timeout=0), name='schema-swagger-ui'),
     path(settings.ADMIN_URL, admin.site.urls),
 ]
 
