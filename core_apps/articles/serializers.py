93ecdd9d1cecd3f125a96acf6b04479e17a3ba59
Jkhall81 <jason.kei.hall@gmail.com>
2023-11-30 17:08:59 -0700

    added everything for bookmark functionality.

--- core_apps/articles/serializers.py
+++ core_apps/articles/serializers.py
@@ -1,6 +1,8 @@
 from rest_framework import serializers
 from core_apps.articles.models import Article, ArticleView
 from core_apps.profiles.serializers import ProfileSerializer
+from core_apps.bookmarks.models import Bookmark
+from core_apps.bookmarks.serializers import BookmarkSerializer
 
 
 class TagListField(serializers.Field):
@@ -28,9 +30,18 @@ class ArticleSerializer(serializers.ModelSerializer):
     tags = TagListField()
     views = serializers.SerializerMethodField()
     average_rating = serializers.ReadOnlyField()
+    bookmarks = serializers.SerializerMethodField()
+    bookmarks_count = serializers.SerializerMethodField()
     created_at = serializers.SerializerMethodField()
     updated_at = serializers.SerializerMethodField()
     
+    def get_bookmarks(self, obj):
+        bookmarks = Bookmark.objects.filter(article=obj)
+        return BookmarkSerializer(bookmarks, many=True).data
+
+    def get_bookmarks_count(self, obj):
+        return Bookmark.objects.filter(article=obj).count()
+
     def get_average_rating(self, obj):
         return obj.average_rating()
     
@@ -72,4 +83,4 @@ class ArticleSerializer(serializers.ModelSerializer):
     
     class Meta:
         model = Article
-        fields = ['id', 'title', 'slug', 'tags', 'estimated_reading_time', 'author_info', 'views', 'description', 'body', 'banner_image', 'average_rating', 'created_at', 'updated_at',]
+        fields = ['id', 'title', 'slug', 'tags', 'estimated_reading_time', 'author_info', 'views', 'description', 'body', 'banner_image', 'average_rating', 'bookmarks_count', 'bookmarks', 'created_at', 'updated_at',]
---
+++ core_apps/bookmarks/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .views import BookmarkCreateView, BookmarkDestroyView
+
+urlpatterns = [
+    path('bookmark_articles/<uuid:article_id>/', BookmarkCreateView.as_view(), name='bookmark_article'),
+    path('remove_bookmark/<uuid:article_id>/', BookmarkDestroyView.as_view(), name='remove_bookmark'),
+]
