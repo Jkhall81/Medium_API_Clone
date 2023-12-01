1b732466b11548228d33c521b4f79d0563a94e23
Jkhall81 <jason.kei.hall@gmail.com>
2023-12-01 15:57:16 -0700

    added elasticsearch for Article model.

--- authors_api/urls.py
+++ authors_api/urls.py
@@ -31,6 +31,7 @@ urlpatterns = [
     path("api/v1/ratings/", include("core_apps.articles.urls")),
     path('api/v1/bookmarks', include('core_apps.bookmarks.urls')),
     path('api/v1/responses', include('core_apps.responses.urls')),
+    path('api/v1/elastic/', include('core_apps.search.urls')),
 ]
 
 admin.site.site_header = "Medium Clone API Admin"
---
+++ core_apps/search/signals.py
@@ -0,0 +1,14 @@
+from django.db.models.signals import post_save, post_delete
+from django.dispatch import receiver
+from django_elasticsearch_dsl.registries import registry
+from core_apps.articles.models import Article
+
+
+@receiver(post_save, sender=Article)
+def update_document(sender, instance=None, created=False, **kwargs):
+    registry.update(instance)
+
+
+@receiver(post_delete, sender=Article)
+def delete_document(sender, instance=None, **kwargs):
+    registry.delete(instance)
