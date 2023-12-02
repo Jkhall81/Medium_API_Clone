f33d3906be366566ec200d50de283e0b745edb27
Jkhall81 <jason.kei.hall@gmail.com>
2023-12-02 12:24:10 -0700

    added test for forms and serializers, showing one hundred percent coverage for user app code.

---
+++ core_apps/users/tests/test_forms.py
@@ -0,0 +1,29 @@
+import pytest
+from core_apps.users.forms import UserCreateForm
+from core_apps.users.tests.factories import UserFactory
+
+@pytest.mark.django_db
+def test_user_creation_form_valid_data():
+    data = {
+        'first_name': 'John',
+        'last_name': 'Doe',
+        'email': 'john.doe@example.com',
+        'password1': 'secure_password_123',
+        'password2': 'secure_password_123',
+    }
+    form = UserCreateForm(data)
+    assert form.is_valid()
+    
+@pytest.mark.django_db
+def test_user_creation_form_invalid_data():
+    user = UserFactory()
+    data = {
+        'first_name': 'John',
+        'last_name': 'Doe',
+        'email': user.email,
+        'password1': 'secure_password_123',
+        'password2': 'secure_password_123',
+    }
+    form = UserCreateForm(data)
+    assert not form.is_valid()
+    assert 'email' in form.errors
--- core_apps/users/views.py
+++ core_apps/users/views.py
@@ -7,7 +7,7 @@ from .serializers import UserSerializer
 
 class CustomUserDetailsView(RetrieveUpdateAPIView):
     serializer_class = UserSerializer
-    permission_classes = IsAuthenticated
+    permission_classes = [IsAuthenticated]
 
     def get_object(self):
         return self.request.user
